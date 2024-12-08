e2e224528c1a70084a7df031790cb8a4bb6b947b
fumio <p30f22g50@gmail.com>
2024-12-08 18:40:28 +0900

    Add randomizeCell

--- src/lib/Binaries.svelte
+++ src/lib/Binaries.svelte
@@ -3,36 +3,43 @@
 
   let cells = $state<Cell[]>([]);
 
-  const initializeNumbers = () => {
+  const initializeCells = (): Cell[] => {
     const columns = Math.floor(window.innerWidth / 20);
     const rows = Math.floor(window.innerHeight / 24);
     const total = columns * rows;
 
-    cells = Array(total)
+    return Array(total)
       .fill(0)
       .map(() => ({
         value: Math.round(Math.random()),
         opacity: Math.random(),
-        delay: Math.random() * 0.5,
+        delay: Math.random(),
       }));
   };
 
-  const updateNumbers = () => {
-    cells = cells.map((n) => ({
-      value: Math.random() > 0.7 ? Math.round(Math.random()) : n.value,
-      opacity: Math.random() > 0.7 ? Math.round(Math.random()) : n.value,
-      delay: n.delay,
-    }));
+  const randomizeCell = (cell: Cell): Cell => {
+    if (Math.random() <= 0.7) {
+      return cell;
+    }
+    return {
+      value: Math.round(Math.random()),
+      opacity: Math.random(),
+      delay: cell.delay,
+    };
+  };
+
+  const updateCells = (cells: Cell[]): Cell[] => {
+    return cells.map((cell) => randomizeCell(cell));
   };
 
   $effect(() => {
-    initializeNumbers();
-    const interval = setInterval(updateNumbers, 100);
+    cells = initializeCells();
+    const interval = setInterval(() => (cells = updateCells(cells)), 100);
     return () => clearInterval(interval);
   });
 </script>
 
-<svelte:window on:resize={initializeNumbers} />
+<svelte:window on:resize={initializeCells} />
 
 <div class="grid grid-cols-[repeat(auto-fill,minmax(20px,1fr))]">
   {#each cells as cell}
